94348630684b697326a87fa50b69af1ee337cc84
Alexander Yanin <a.yanin@g.nsu.ru>
2023-01-21 02:49:47 +0700

    Switch from nodejs to v8
    In the previous commit's message I was talking about how deno seemed
    like a right choice for the JS runtime to use in codegen tests.
    Well, I've looked more closely into that and realized I don't actually
    need anything that deno offers and instead I should use V8 directly.
    Which is what I've ultimately opted into.
    
    I'm guessing I didn't need to abuse the global environment as much as I
    did here, but it works well enough for me anyway.

--- tests/codegen.rs
+++ tests/codegen.rs
@@ -1,19 +1,12 @@
 mod common;
 
-use std::fs::File;
-use std::io::Write;
-use std::os::fd::{AsRawFd, FromRawFd, OwnedFd};
+use std::panic::{self, AssertUnwindSafe};
 use std::path::Path;
-use std::process::{Command, Stdio};
-use std::thread::scope;
-use std::time::Duration;
 
-use command_fds::{CommandFdExt, FdMapping};
-use nix::fcntl::{FcntlArg, FdFlag};
+use once_cell::sync::Lazy;
 use paste::paste;
 use pretty_assertions::assert_str_eq;
 
-use process_control::{ChildExt, Control};
 use wacc::analysis::{self, TypeChecker, TypeckResult};
 use wacc::codegen::ctx::FieldTable;
 use wacc::codegen::passes as cg_passes;
@@ -23,84 +16,259 @@ use wacc::source::{Source, SourceBuffer};
 
 use self::common::Dump;
 
-fn run_wasm(wasm: &[u8], input: &[u8]) -> Dump<'static> {
-    static RUN_TEST_JS: &[u8] = include_bytes!("codegen/run-test.js");
-    static TEST_RUN_LIMIT: Duration = Duration::from_secs(5 * 60);
+fn run_wasm(wasm: Vec<u8>, input: &'static [u8]) -> Dump<'static> {
+    const OUTPUT_LINES_KEY: &str = "__outputLines";
 
-    let (wasm_pipe_rd, wasm_pipe_wr) = nix::unistd::pipe().unwrap();
+    static RUN_TEST_JS: &str = include_str!("codegen/run-test.js");
+    static V8_PLATFORM: Lazy<v8::SharedRef<v8::Platform>> = Lazy::new(|| {
+        v8::V8::set_flags_from_string("--experimental-wasm-gc");
 
-    // Safety: we've just made ourselves a brand new pipe,
-    // so both fds are definitely valid here, and `close` is enough in terms of bookkeeping
-    let wasm_pipe_wr = unsafe { OwnedFd::from_raw_fd(wasm_pipe_wr) };
-    let wasm_pipe_rd = unsafe { OwnedFd::from_raw_fd(wasm_pipe_rd) };
+        let platform = v8::new_default_platform(0, false).make_shared();
+        v8::V8::initialize_platform(platform.clone());
+        v8::V8::initialize();
+        v8::V8::assert_initialized();
 
-    let (input_pipe_rd, input_pipe_wr) = nix::unistd::pipe().unwrap();
-
-    // Safety: same as above
-    let input_pipe_wr = unsafe { OwnedFd::from_raw_fd(input_pipe_wr) };
-    let input_pipe_rd = unsafe { OwnedFd::from_raw_fd(input_pipe_rd) };
-
-    // make sure the child doesn't haunt pipe_wr
-    nix::fcntl::fcntl(
-        wasm_pipe_wr.as_raw_fd(),
-        FcntlArg::F_SETFD(FdFlag::FD_CLOEXEC),
-    )
-    .unwrap();
-    nix::fcntl::fcntl(
-        input_pipe_wr.as_raw_fd(),
-        FcntlArg::F_SETFD(FdFlag::FD_CLOEXEC),
-    )
-    .unwrap();
-
-    let mut child = Command::new("node")
-        .arg("--experimental-wasm-gc")
-        .arg("--input-type=module")
-        .arg("-")
-        .stdin(Stdio::piped())
-        .stderr(Stdio::inherit())
-        .stdout(Stdio::piped())
-        .fd_mappings(vec![
-            FdMapping {
-                parent_fd: wasm_pipe_rd.as_raw_fd(),
-                child_fd: 3,
-            },
-            FdMapping {
-                parent_fd: input_pipe_rd.as_raw_fd(),
-                child_fd: 4,
-            },
-        ])
-        .unwrap()
-        .spawn()
-        .unwrap();
-
-    // gotta close what we don't need
-    drop(wasm_pipe_rd);
-    drop(input_pipe_rd);
-
-    child.stdin.take().unwrap().write_all(RUN_TEST_JS).unwrap();
-
-    let out = scope(|s| {
-        s.spawn(|| {
-            File::from(wasm_pipe_wr).write_all(wasm).unwrap();
-            File::from(input_pipe_wr).write_all(input).unwrap();
-        });
-
-        match child
-            .controlled_with_output()
-            .time_limit(TEST_RUN_LIMIT)
-            .strict_errors()
-            .terminate_for_timeout()
-            .wait()
-            .unwrap()
-        {
-            Some(out) => out,
-            None => panic!("The child process timed out"),
-        }
+        platform
     });
 
-    assert!(out.status.success());
+    Lazy::force(&V8_PLATFORM);
 
-    out.stdout.into()
+    let isolate = &mut v8::Isolate::new(Default::default());
+
+    let scope = &mut v8::HandleScope::new(isolate);
+    let ctx = v8::Context::new(scope);
+    let scope = &mut v8::ContextScope::new(scope, ctx);
+
+    let wasm_backing_store = v8::ArrayBuffer::new_backing_store_from_vec(wasm).make_shared();
+    let wasm_array_buf = v8::ArrayBuffer::with_backing_store(scope, &wasm_backing_store);
+    let wasm_array =
+        v8::Uint8Array::new(scope, wasm_array_buf, 0, wasm_backing_store.len()).unwrap();
+    let wasm_key = v8::String::new(scope, "wasm").unwrap();
+
+    scope
+        .get_current_context()
+        .global(scope)
+        .set(scope, wasm_key.into(), wasm_array.into())
+        .unwrap();
+
+    let input_lines: Vec<_> = input
+        .split(|&c| c == b'\n')
+        .map(|line| {
+            let line_backing_store =
+                v8::ArrayBuffer::new_backing_store_from_vec(line.into()).make_shared();
+            let line_array_buf = v8::ArrayBuffer::with_backing_store(scope, &wasm_backing_store);
+
+            v8::Uint8Array::new(scope, line_array_buf, 0, line_backing_store.len())
+                .unwrap()
+                .into()
+        })
+        .collect();
+    let input_lines = v8::Array::new_with_elements(scope, input_lines.as_slice());
+    let input_key = v8::String::new(scope, "inputLines").unwrap();
+
+    scope
+        .get_current_context()
+        .global(scope)
+        .set(scope, input_key.into(), input_lines.into())
+        .unwrap();
+
+    fn js_write_to_stdout<'a>(
+        scope: &mut v8::HandleScope<'a>,
+        args: v8::FunctionCallbackArguments<'a>,
+        mut ret: v8::ReturnValue,
+    ) {
+        let res = panic::catch_unwind({
+            let mut scope = AssertUnwindSafe(&mut *scope);
+
+            move || {
+                let scope = &mut **scope;
+                let output_lines_key = v8::String::new(scope, OUTPUT_LINES_KEY).unwrap();
+                let output_lines = scope
+                    .get_current_context()
+                    .global(scope)
+                    .get(scope, output_lines_key.into())
+                    .unwrap();
+                let output_lines = v8::Local::<v8::Array>::try_from(output_lines).unwrap();
+
+                let line = args.get(0);
+                assert!(line.is_uint8_array());
+                output_lines
+                    .set_index(scope, output_lines.length(), line)
+                    .unwrap();
+            }
+        });
+
+        ret.set_undefined();
+
+        if res.is_err() {
+            // the panic hooks have already been executed at this point,
+            // one of which prints the panic message
+            scope.terminate_execution();
+        }
+    }
+
+    fn js_encode_utf8<'a>(
+        scope: &mut v8::HandleScope<'a>,
+        args: v8::FunctionCallbackArguments<'a>,
+        mut ret: v8::ReturnValue,
+    ) {
+        let res = panic::catch_unwind({
+            let mut scope = AssertUnwindSafe(&mut *scope);
+
+            move || {
+                let scope = &mut **scope;
+
+                let string = match v8::Local::<v8::String>::try_from(args.get(0)) {
+                    Ok(string) => string,
+                    Err(e) => {
+                        let msg = v8::String::new(scope, &e.to_string()).unwrap();
+                        let exc = v8::Exception::type_error(scope, msg);
+                        scope.throw_exception(exc);
+
+                        return;
+                    }
+                };
+
+                let string: Vec<_> = string.to_rust_string_lossy(scope).bytes().collect();
+                let backing_store =
+                    v8::ArrayBuffer::new_backing_store_from_vec(string).make_shared();
+                let array_buf = v8::ArrayBuffer::with_backing_store(scope, &backing_store);
+                let array =
+                    v8::Uint8Array::new(scope, array_buf, 0, array_buf.byte_length()).unwrap();
+
+                ret.set(array.into());
+            }
+        });
+
+        if res.is_err() {
+            scope.terminate_execution();
+        }
+    }
+
+    fn js_decode_utf8<'a>(
+        scope: &mut v8::HandleScope<'a>,
+        args: v8::FunctionCallbackArguments<'a>,
+        mut ret: v8::ReturnValue,
+    ) {
+        let res = panic::catch_unwind({
+            let mut scope = AssertUnwindSafe(&mut *scope);
+
+            move || {
+                let scope = &mut **scope;
+
+                let array = match v8::Local::<v8::Uint8Array>::try_from(args.get(0)) {
+                    Ok(array) => array,
+                    Err(e) => {
+                        let msg = v8::String::new(scope, &e.to_string()).unwrap();
+                        let exc = v8::Exception::type_error(scope, msg);
+                        scope.throw_exception(exc);
+
+                        return;
+                    }
+                };
+
+                let array: Vec<_> = array
+                    .buffer(scope)
+                    .unwrap()
+                    .get_backing_store()
+                    .iter()
+                    .map(|cell| cell.get())
+                    .collect();
+                let string = String::from_utf8_lossy(&array);
+                let string = v8::String::new(scope, &string).unwrap();
+
+                ret.set(string.into());
+            }
+        });
+
+        if res.is_err() {
+            scope.terminate_execution();
+        }
+    }
+
+    let output_lines_key = v8::String::new(scope, OUTPUT_LINES_KEY).unwrap();
+    let output_lines_value = v8::Array::new(scope, 0);
+
+    scope
+        .get_current_context()
+        .global(scope)
+        .set(scope, output_lines_key.into(), output_lines_value.into())
+        .unwrap();
+
+    let write_to_stdout_cb = v8::FunctionBuilder::<v8::Function>::new(js_write_to_stdout)
+        .build(scope)
+        .unwrap();
+    let write_to_stdout_key = v8::String::new(scope, "writeToStdout").unwrap();
+
+    scope
+        .get_current_context()
+        .global(scope)
+        .set(scope, write_to_stdout_key.into(), write_to_stdout_cb.into())
+        .unwrap();
+
+    let encode_utf8_cb = v8::FunctionBuilder::<v8::Function>::new(js_encode_utf8)
+        .build(scope)
+        .unwrap();
+    let encode_utf8_key = v8::String::new(scope, "encodeUtf8").unwrap();
+
+    scope
+        .get_current_context()
+        .global(scope)
+        .set(scope, encode_utf8_key.into(), encode_utf8_cb.into())
+        .unwrap();
+
+    let decode_utf8_cb = v8::FunctionBuilder::<v8::Function>::new(js_decode_utf8)
+        .build(scope)
+        .unwrap();
+    let decode_utf8_key = v8::String::new(scope, "decodeUtf8").unwrap();
+
+    scope
+        .get_current_context()
+        .global(scope)
+        .set(scope, decode_utf8_key.into(), decode_utf8_cb.into())
+        .unwrap();
+
+    let code = v8::String::new(scope, RUN_TEST_JS).unwrap();
+    let script = v8::Script::compile(scope, code, None).unwrap();
+
+    let scope = &mut v8::TryCatch::new(scope);
+    let result = script.run(scope)
+        .and_then(|value| v8::Local::<v8::Promise>::try_from(value).ok());
+
+    if let Some(promise) = result {
+        while promise.state() == v8::PromiseState::Pending {
+            v8::Platform::pump_message_loop(&V8_PLATFORM, scope, true);
+            scope.perform_microtask_checkpoint();
+        }
+    }
+
+    if let Some(exc) = scope.exception() {
+        let msg = exc
+            .to_detail_string(scope)
+            .unwrap()
+            .to_rust_string_lossy(scope);
+        panic!("The script has thrown an uncaught exception: {}", msg);
+    }
+
+    let output_lines = scope
+        .get_current_context()
+        .global(scope)
+        .get(scope, output_lines_key.into())
+        .unwrap();
+    let output_lines = v8::Local::<v8::Array>::try_from(output_lines).unwrap();
+    let output_lines: Vec<_> = (0..output_lines.length())
+        .map(|i| output_lines.get_index(scope, i).unwrap())
+        .map(|local| v8::Local::<v8::Uint8Array>::try_from(local).unwrap())
+        .collect();
+    let output_size: usize = output_lines.iter().map(|array| array.byte_length()).sum();
+    let mut output = vec![0; output_size];
+    let mut written_size = 0;
+
+    for output_line in output_lines {
+        written_size += output_line.copy_contents(&mut output[written_size..]);
+    }
+
+    output.into()
 }
 
 fn run_codegen_test_inner(code: &'static [u8], expected: &'static [u8], filename: &'static str) {
@@ -158,7 +326,7 @@ fn run_codegen_test_inner(code: &'static [u8], expected: &'static [u8], filename
     );
     let wasm = codegen_out.module.encode().unwrap();
 
-    let actual = run_wasm(&wasm, &[]);
+    let actual = run_wasm(wasm, &[]);
     let expected = Dump::from(expected);
 
     assert_str_eq!(actual, expected);
--- tests/codegen/run-test.js
+++ tests/codegen/run-test.js
@@ -1,75 +1,19 @@
-// XXX: This script assumes:
-// - fd 3 is mapped to a pipe that provides wasm bytes
-// - fd 4 is mapped to a pipe that provides UTF-8-encoded input
-
-import * as fs from 'node:fs';
-
-const WASM_FD = 3;
-const INPUT_FD = 4;
-
-let inputBuf = Buffer.alloc(0);
-let inputEof = false;
+let inputLineIdx = 0;
 
 function readTestInputLine() {
-  if (inputEof) {
+  if (inputLineIdx >= inputLines.length) {
     return new Int8Array();
-  };
-
-  let bufs = [inputBuf];
-  let prevSegmentLen = 0;
-  let line;
-
-  while (true) {
-    const last = bufs[bufs.length - 1];
-    const nlIndex = last.indexOf(0x0a);
-
-    if (nlIndex !== -1) {
-      // the `\n` is chopped off
-      line = Buffer.concat(bufs, prevSegmentLen + nlIndex);
-      inputBuf = last.subarray(nlIndex + 1);
-
-      break;
-    }
-
-    const buf = Buffer.alloc(1024);
-    const bytesRead = fs.readSync(INPUT_FD, buf);
-
-    if (bytesRead === 0) {
-      line = Buffer.concat(bufs);
-      inputBuf = Buffer.alloc(0);
-      inputEof = true;
-
-      break;
-    }
-
-    prevSegmentLen += last.length;
-    bufs.push(buf.subarray(0, bytesRead));
   }
 
-  return new Int8Array(line.buffer, line.byteOffset, line.length);
+  return inputLines[inputLineIdx++];
 }
 
-function writeToStdout(array) {
-  for (let offset = 0; offset < array.length;) {
-    const bytesWritten = fs.writeSync(process.stdout.fd, array, offset);
-
-    if (bytesWritten === 0) {
-      console.error("writeSync returned 0");
-
-      process.exit(10);
-    }
-
-    offset += bytesWritten;
-  }
-}
-
-// Decodes as UTF-8.
 function arrayToString(array) {
-  return new TextDecoder().decode(array);
+  return decodeUtf8(array);
 }
 
 function stringToArray(str) {
-  return new TextEncoder().encode(str);
+  return encodeUtf8(str);
 }
 
 let exports;
@@ -99,9 +43,7 @@ function arrayToBytes(buf) {
 const imports = {
   "cool-runtime": {
     abort: () => {
-      console.error("the program under test has called `abort`");
-
-      process.exit(2);
+      throw "the program under test has called `abort`";
     },
 
     print_bytes: (bytes) => {
@@ -136,29 +78,6 @@ const imports = {
     },
   },
 };
-
-function readWasmModule() {
-  let bufs = [];
-  let size = 0;
-
-  while (true) {
-    const buf = Buffer.alloc(8192);
-    const bytesRead = fs.readSync(WASM_FD, buf);
-
-    if (bytesRead === 0) {
-      break;
-    }
-
-    bufs.push(buf.subarray(0, bytesRead));
-    size += bytesRead;
-  }
-
-  fs.closeSync(WASM_FD);
-
-  return Buffer.concat(bufs, size);
-}
-
-const wasm = readWasmModule();
 
 WebAssembly.instantiate(wasm, imports).then((module) => {
   exports = module.instance.exports;
