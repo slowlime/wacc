ea36a76f11f7f7cb943d37f4ada335db27053383
Alexander Yanin <a.yanin@g.nsu.ru>
2023-02-18 00:11:05 +0700

    Resize the terminal automatically to fit the available area

--- web/src/index.js
+++ web/src/index.js
@@ -1,12 +1,26 @@
-import { Terminal } from 'xterm';
 import ace from 'ace-builds';
+import { Terminal } from 'xterm';
+import { FitAddon } from 'xterm-addon-fit';
 
 import 'xterm/css/xterm.css';
 
 import './styles.css';
 
-const term = new Terminal();
-term.open(document.getElementById('terminal'));
-term.write('Hey there.');
-
+let terminal;
 const editor = ace.edit('editor-inner');
+
+function initTerminal() {
+  const element = document.getElementById('terminal');
+  terminal = new Terminal({ cols: 2, rows: 1 });
+  const termFit = new FitAddon();
+  terminal.loadAddon(termFit);
+  terminal.open(element);
+  termFit.fit();
+
+  document.getElementsByTagName('body')[0].onresize = () => {
+    termFit.fit();
+  };
+}
+
+initTerminal();
+terminal.write('Hey there.');
